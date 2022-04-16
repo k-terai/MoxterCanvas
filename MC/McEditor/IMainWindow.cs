7b30cfa17979e28c16a2db6d322cb6240d622982
k-terai <chibi201028@gmail.com>
2022-04-17 03:13:56 +0900

    Add IMainWindow

---
+++ MC/McEditor/IMainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McEditor
+{
+    internal interface IMainWindow
+    {
+    }
+}
