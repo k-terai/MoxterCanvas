c5032a17a7d4c63d22496b4476dee822be6d13ab
k-terai <chibi201028@gmail.com>
2022-04-24 01:46:04 +0900

    [Editor] Add language resources

---
+++ MC/McEditor/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+
+using McEdShare.CoreSystem;
+using McEdShare.WindowSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McEditor
+{
+    public class MainWindowViewModel : WindowViewModel
+    {
+        public DelegateCommand ChangeLanguageCommand { get; set; }
+
+        public MainWindowViewModel() : base()
+        {
+            InitCommand();
+        }
+
+        private void InitCommand()
+        {
+            ChangeLanguageCommand = new DelegateCommand((object p) =>
+            {
+                switch ((string)p)
+                {
+                    case ResourceService.ENGLISHCODE:
+                        ResourceService.Current.ChangeCulture(ResourceService.ENGLISHCODE);
+                        break;
+
+                    case ResourceService.JAPANACECODE:
+                        ResourceService.Current.ChangeCulture(ResourceService.JAPANACECODE);
+                        break;
+                }
+            });
+        }
+    }
+}
---
+++ MC/McEditor/Service/ResourceService.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using McEdShare.CoreSystem;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McEditor
+{
+    public class ResourceService : NotifyPropertyChangedBase
+    {
+        public const string JAPANACECODE = "ja-jp";
+        public const string ENGLISHCODE = "en-us";
+
+        private static readonly ResourceService _current = new ResourceService();
+
+        private readonly Resources _resources = new Resources();
+
+        public static ResourceService Current
+        {
+            get => _current;
+        }
+
+        public Resources Resources
+        {
+            get { return _resources; }
+        }
+
+        public void ChangeCulture(string name)
+        {
+            Resources.Culture = CultureInfo.GetCultureInfo(name);
+            NotifyPropertyChanged("Resources");
+        }
+    }
+}
