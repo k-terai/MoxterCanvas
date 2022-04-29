0249642b1c0126e30af7bc41bcbb79c5a008d5c4
k-terai <chibi201028@gmail.com>
2022-04-29 17:14:30 +0900

    [Editor] Add CanvasControl classes

---
+++ MC/McEditor/Canvas/CanvasControlViewModel.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using McEdShare.ControlSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McEditor
+{
+    public class CanvasControlViewModel : ControlViewModel
+    {
+
+    }
+}
