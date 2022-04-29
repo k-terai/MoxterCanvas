5557851a59fa7ed4c8e3a54cdb293a1a75cfbbfa
k-terai <chibi201028@gmail.com>
2022-04-29 17:12:30 +0900

    [EdShare] Add ControlSystem

---
+++ MC/McEdShare/ControlSystem/IControl.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if WPF
+using System.Windows.Controls;
+#endif
+
+namespace McEdShare.ControlSystem
+{
+    public interface IControl
+    {
+#if WPF
+        UserControl Control { get; }
+#endif
+    }
+}
