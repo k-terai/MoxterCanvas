4480ff2f70306db24265ea4e0c360a4996b1b4bd
k-terai <chibi201028@gmail.com>
2022-05-03 19:09:28 +0900

    [Editor] Add Utility classes

---
+++ MC/McEditor/Utilities/Save/ISaveFileDialog.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McEditor.Utilities
+{
+    public interface ISaveFileDialog
+    {
+        /// <summary>
+        /// Show dialog.
+        /// </summary>
+        /// <param name="title">Dialog title.</param>
+        /// <param name="filter">Save file filter.</param>
+        /// <returns>Save file path.</returns>
+        string ShowFileDialog(string title, string filter);
+    }
+}
