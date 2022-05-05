c07a482bc545be8049092f06ced133184cc5780e
k-terai <chibi201028@gmail.com>
2022-05-05 21:17:35 +0900

    [EdShare] Add Serialization classes

---
+++ MC/McEdShare/CoreSystem/SerializableBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace McEdShare.CoreSystem
+{
+    [DataContract]
+    public abstract class SerializableBase : NotifyPropertyChangedBase
+    {
+        /// <summary>
+        /// Data serialize version.
+        /// </summary>
+        [DataMember]
+        public uint Version { get; set; }
+    }
+}
---
+++ MC/McEdShare/Serialization/SerializeAttribute.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McEdShare.Serialization
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SerializeAttribute : Attribute
+    {
+        public enum SerializeType
+        {
+            Json
+        }
+
+        public SerializeType Type { get; private set; }
+
+        public SerializeAttribute(SerializeType type)
+        {
+            Type = type;
+        }
+    }
+}
