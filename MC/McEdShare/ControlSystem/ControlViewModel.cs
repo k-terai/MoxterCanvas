a94c63738de1483b8817df330ecde848f4396f98
k-terai <chibi201028@gmail.com>
2022-07-10 02:50:16 +0900

    [McEdShare] Add ControlSystem

---
+++ MC/McEdShare/ControlSystem/ControlViewModel.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2022 K.T
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using McEdShare.CoreSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McEdShare.ControlSystem
+{
+    internal class ControlViewModel : ViewModelBase
+    {
+        private string _title;
+        private bool _enableToolBar;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool EnableToolBar
+        {
+            get => _enableToolBar;
+            set
+            {
+                _enableToolBar = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public ControlViewModel() : base()
+        {
+            Title = string.Empty;
+            EnableToolBar = true;
+        }
+    }
+}
