4266c6600188216f7b99bd03d738fbee7d6fbc35
k-terai <chibi201028@gmail.com>
2022-07-10 03:49:50 +0900

    [Editor] Add resources system.

--- MC/McEditor/Windows/Main/MainWindow.xaml.cs
+++ MC/McEditor/Windows/Main/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2022 K.T
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using McEditor.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
